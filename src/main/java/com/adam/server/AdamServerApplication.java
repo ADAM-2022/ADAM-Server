baa52196efc08f8b1e1d9680eff17059c9296194
ckyeon <qsky8520@gmail.com>
2023-02-19 20:22:11 +0900

    feat(project): setup default project

---
+++ src/main/java/com/adam/server/AdamServerApplication.java
@@ -0,0 +1,13 @@
+package com.adam.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamServerApplication.class, args);
+	}
+
+}
